bf7e7f9218a5b1976d5ed1bfed59a17d9f5bd5d8
Navi0906 <naam0906.be21@chitkara.edu.in>
2025-02-28 20:42:09 +0530

    Naam Simran Singh Add: Sam's average marks

---
+++ SamAverageMarks.java
@@ -0,0 +1,10 @@
+class SamAverageMarks{
+	public static void main(String[] args){
+		int maths = 94;
+		int physics = 95;
+		int chemistry = 96;
+		
+		int average = (maths + physics + chemistry)/3;
+		System.out.print("Sam's average mark in PCM is "+average);
+	}
+}
