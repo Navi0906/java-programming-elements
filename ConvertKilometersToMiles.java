f1ad69abd1752ce8c62d954712ccf4d97265837d
Navi0906 <naam0906.be21@chitkara.edu.in>
2025-03-01 10:01:35 +0530

    Naam Simran Singh Delete: Level-1-programs

--- ConvertKilometersToMiles.java
+++
@@ -1,8 +0,0 @@
-class ConvertKilometersToMiles{
-	public static void main(String[] args){
-		double kilometers = 10.8;
-		double toMiles = kilometers*1.6;
-		
-		System.out.print("The distance "+kilometers+" km in miles is "+toMiles);
-	}
-}
--- HarryAge.java
+++
@@ -1,8 +0,0 @@
-class HarryAge{
-	public static void main(String[] args){
-		int birth = 2000;
-		int current = 2024;
-		int age = current - birth;
-		System.out.print("Harry's age in 2024 is "+age);
-	}
-}
